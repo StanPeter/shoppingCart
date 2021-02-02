640568c1adcf2d1166dd43c79680086ac1512259
StanPeter <stanislav.petr7@gmail.com>
2021-02-02 02:37:04 +0100

    fixed side bar, app more or less finished

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -14,23 +14,27 @@ type Props = {
     removeFromCart: (id: number) => void;
 };
 
-const Cart: FC<Props> = ({ cartItems, addToCart, removeFromCart }) => (
+const Cart: FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+    const calculateTotal = (items: CartItemType[]) =>
+        items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
 
-    <div className={styles.wrapper}>
+    return (
+        <div className={styles.wrapper}>
 
-        <h2>Your Shopping Cart</h2>
-        {cartItems.length > 0
-            ? cartItems.map(item => (
-                <CartItem
-                    cart={item}
-                    addToCart={addToCart}
-                    removeFromCart={removeFromCart}
-                    key={item.id} />
-            ))
-            : <p>No items in cart</p>
-        }
-        
-    </div>
-);
+            <h2>Your Shopping Cart</h2>
+            {cartItems.length > 0
+                ? cartItems.map(item => (
+                    <CartItem
+                        cart={item}
+                        addToCart={addToCart}
+                        removeFromCart={removeFromCart}
+                        key={item.id} />
+                ))
+                : <p>No items in cart</p>
+            }
+            <h2>Total ${calculateTotal(cartItems).toFixed(2)}</h2>
+        </div>
+    );
+};
 
 export default Cart;
--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -16,33 +16,36 @@ type Props = {
 const CartItem: FC<Props> = ({ cart, addToCart, removeFromCart }) => (
 
     <div className={styles.wrapper}>
+        <div>
 
-        <h3>{cart.title}</h3>
+            <h3>{cart.title}</h3>
 
-        <div className="information">
-            <p>Price: ${cart.price}</p>
-            <p>Total: ${(cart.amount * cart.price).toFixed(2)}</p>
-        </div>
+            <div className={styles.information}>
+                <p>Price: ${cart.price}</p>
+                <p>Total: ${(cart.amount * cart.price).toFixed(2)}</p>
+            </div>
 
-        <div className="buttons">
-            <p>{cart.amount}</p>
-            <Button
-                // size='small'
-                disableElevation
-                variant='contained'
-                onClick={() => removeFromCart(cart.id)} >
-                -
+            <div className={styles.buttons}>
+                <p>{cart.amount}</p>
+                <Button
+                    size='small'
+                    disableElevation
+                    variant='contained'
+                    onClick={() => removeFromCart(cart.id)} >
+                    -
             </Button>
-            <p>{cart.amount}</p>
-            <Button
-                // size='small'
-                disableElevation
-                variant='contained'
-                onClick={() => addToCart(cart)} >
-                +
+                <p>{cart.amount}</p>
+                <Button
+                    size='small'
+                    disableElevation
+                    variant='contained'
+                    onClick={() => addToCart(cart)} >
+                    +
             </Button>
+            </div>
+            <img src={cart.image} alt={cart.title} />
+
         </div>
-        <img src={cart.image} alt={cart.title} />
     </div>
 );
 
